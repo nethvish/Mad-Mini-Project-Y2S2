aa5d48191438cf5c868f33db6141b9ec509c518a
pawan-live <pawan.senpura@gmail.com>
2023-05-14 06:51:51 +0530

    :sparkles: add buyer post create xml

---
+++ app/src/main/java/com/example/agroceylon/models/BuyerModel.kt
@@ -0,0 +1,12 @@
+package com.example.agroceylon.models
+
+data class BuyerPostModel(
+    var PostId: String? = null,
+    var postTitle: String? = null,
+    var postDate: String? = null,
+    var postPrice: String? = null,
+    var postLocation: String? = null,
+    var postType: String? = null,
+    var postQty: String? = null,
+    var postName: String? = null
+)
